22aaea56418d8a77b549a8a6dd17782c692c580e
bobjgruber <34726580+bobjgruber@users.noreply.github.com>
2019-11-10 16:43:13 -0500

    updating to include wedding website

---
+++ email.js
@@ -0,0 +1,5 @@
+var username = "rgruber";
+var hostname = "springfieldcollege.edu";
+var linktext = username + "@" + hostname ;
+document.write("<a href='" + "mail" + "to:" + username
++ "@" + hostname + "'>" + linktext + "</a>");
